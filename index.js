24e792af8964193bfd58cdbe43e23e3f7a41581f
haikavaks@gmail.com <hayk@intelinair.com>
2020-08-03 23:38:43 +0400

    start

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express();
-
+const jwt = require('jsonwebtoken')
 const PORT = process.env.PORT || 80;
 
-app.get('/',(req,res)=>{
-  res.end('<h1>home</h1>')
+app.get('/login',(req,res)=>{
+  res.end('<h1>home4</h1>')
 })
 
-app.get('/about',(req,res)=>{
-  res.end('<h1>ABout</h1>')
+app.post('/login',(req,res)=>{
+  console.log(req.body)
+  const userName = req.body.userName
+  const user = {name : userName}
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json ({accessToken})
 })
+
 app.listen(PORT, ()=>{
   console.log('here');
 })
